2684ca82b3f1921dcd759b7e8de167e39fb28cc7
rfnajid <ridhaaaaazis@gmail.com>
2015-02-15 12:12:17 +0700

    Project Commit
    File Assets commit

---
+++ Assets/Char.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Char : MonoBehaviour {
+	
+	public float speed=0.3f;
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("left")){
+			MoveLeft();
+		}
+		
+		else if(Input.GetKey("right")){
+			MoveRight();
+		}
+		else {
+			Standby();
+		}
+	}
+
+	void MoveRight(){
+		gameObject.transform.Translate(new Vector3(speed,0,0));
+		gameObject.GetComponent<Animator>().SetBool("Walk",true);
+		gameObject.transform.rotation=Quaternion.Euler(0,0,0);
+		print ("right");
+	}
+	
+	void MoveLeft(){
+		gameObject.transform.Translate(new Vector3(speed,0,0));
+		gameObject.GetComponent<Animator>().SetBool("Walk",true);
+		gameObject.transform.rotation=Quaternion.Euler(0,-180,0);
+		print ("left");
+	}	
+
+	void Standby(){
+		gameObject.GetComponent<Animator>().SetBool("Walk",false);
+	}
+}
